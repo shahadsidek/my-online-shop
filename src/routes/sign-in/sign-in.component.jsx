9a6ac6380f408824cca597fe12a00f1d6a2e3e5b
Shahad  Sidek <73951870+shahadsidek@users.noreply.github.com>
2023-01-06 16:46:55 +0300

    created the sign in component with firebase

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,19 @@
+import { signInWithGooglePopup } from '../../utils/firebase/firebase.utils';
+
+
 const SignIn = () => {
+
+    const logGoogleUser = async () => {
+        const response = await signInWithGooglePopup();
+        console.log(response);
+    };
+
     return (
         <div>
             <h1>This is the sign in page</h1>
+            <button onClick={logGoogleUser}>
+                Sign In With Google
+            </button>
         </div>
     )
 }
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,8 +1,6 @@
 /* this is a JS file because we are not returning any JSX element. */
 import { initializeApp } from 'firebase/app';
-import {getAuth, signInWithPopup, signInWithRedirect, GoogleAuthProvider} from 'firebase/auth';
-
-
+import {getAuth, signInWithPopup, signInWithRedirect, GoogleAuthProvider, onAuthStateChanged} from 'firebase/auth';
 
 
 // Your web app's Firebase configuration
@@ -15,12 +13,20 @@ const firebaseConfig = {
     appId: "1:738828789689:web:299068ea65104643fab868"
 };
 
+
+/* getAuth returns a promise --> get promise means we are going to use .then return a unique
+ user id and ane mail   */
+
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
 
 // setting up Google Auth - first we need to set the provider
 const provider = new GoogleAuthProvider();
+
 provider.setCustomParameters({
     prompt: 'select_account'
-})
+});
+
+export const auth = getAuth(); // retrieves the current authentication of the user.
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
